06d09a5dcbee0ec88681b452643f1a4b42b4851a
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2022-08-08 16:47:59 +0500

    products-table

---
+++ src/Components/js/ProductsTable.js
@@ -0,0 +1,12 @@
+export default function ProductsTable() {
+    return (
+        <>
+            <div className="products-table">
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </>
+    )
+}
